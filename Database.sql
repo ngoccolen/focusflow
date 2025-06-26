f48c75f9051a16dfaf2ce64687f8f7ef4c7f413f
chill IT girl <lenguyenngoc2006qb@gmail.com>
2025-06-26 16:59:10 +0700

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -31,7 +31,12 @@ CREATE TABLE videos (
 );
 
 ALTER TABLE songs ADD CONSTRAINT unique_song_path_per_user UNIQUE (file_path, id);
-
+CREATE TABLE quotes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 CREATE TABLE task (
     task_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
@@ -54,4 +59,52 @@ CREATE TABLE note (
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (task_id) REFERENCES task(task_id) ON DELETE SET NULL
 );
+ALTER TABLE task ADD COLUMN remind_at DATETIME NULL;
+CREATE TABLE study_time (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    study_date DATE NOT NULL,
+    hours DECIMAL(10, 8) NOT NULL);
+CREATE TABLE friendships (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    friend_id INT NOT NULL,
+    status ENUM('pending', 'accepted', 'rejected') DEFAULT 'pending',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE KEY unique_friendship (user_id, friend_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (friend_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- Bảng chat_conversations
+CREATE TABLE chat_conversations (
+    chat_id INT AUTO_INCREMENT PRIMARY KEY,
+    is_group BOOLEAN,
+    chat_name VARCHAR(100),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Bảng chat_members
+CREATE TABLE chat_members (
+    chat_id INT,
+    user_id INT,
+    joined_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (chat_id, user_id),
+    FOREIGN KEY (chat_id) REFERENCES chat_conversations(chat_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- Bảng chat_messages
+CREATE TABLE chat_messages (
+    message_id INT AUTO_INCREMENT PRIMARY KEY,
+    chat_id INT,
+    sender_id INT,
+    content TEXT,
+    type ENUM('text', 'image', 'file', 'audio', 'emoji'),
+    file_path VARCHAR(255),
+    sent_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (chat_id) REFERENCES chat_conversations(chat_id) ON DELETE CASCADE,
+    FOREIGN KEY (sender_id) REFERENCES users(id) ON DELETE CASCADE
+);
+ALTER TABLE chat_conversations ADD COLUMN group_name VARCHAR(255)
 
